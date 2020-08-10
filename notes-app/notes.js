1e1dc7f8986b31e15627ea804072eeaa5966882e
Kerry Smith <kerrysfs@gmail.com>
2020-08-10 15:36:11 -0700

    simple note node js app add , list, delete, and read 90%

---
+++ notes-app/notes.js
@@ -0,0 +1,69 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+const getNotes = () => {
+    return 'Your notes...'
+}
+
+const addNote = (title, body) => {
+    const notes = loadNotes()
+    // const duplicateNotes = notes.filter((note) => note.title === title ) 
+    const duplicateNote = notes.find((note) => note.title === title)
+
+    if (!duplicateNote) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log(chalk.green.inverse('New note added!'))
+    } else {
+        console.log(chalk.red.inverse('Note title taken!'))
+    }
+}
+
+
+const removeNote = (title) => {
+   const notes = loadNotes()
+   const notesToKeep = notes.filter((note) => note.title !== title)
+
+   if(notes.length > notesToKeep.length){
+    console.log(chalk.green.inverse('Note Removed!'))
+    saveNotes(notesToKeep)
+   }else {
+       console.log(chalk.red.inverse('No note found..sorry try again'))
+   }
+}
+
+
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+const listNotes = () => {
+  console.log(chalk.inverse('Your notes'))
+  const notes = loadNotes()
+  notes.forEach((note) =>{
+      console.log(chalk.red.inverse(note.title))
+  })
+}
+
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes
+}

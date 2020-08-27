d524434ec25962ad4dd34b731da0e59abf25b51e
Kerry Smith <kerrysfs@gmail.com>
2020-08-26 23:36:55 -0700

    wired up submit button to fetch from user input location

--- webserver/public/js/app.js
+++ webserver/public/js/app.js
@@ -6,14 +6,39 @@
 //   })
 // })
 
-fetch('http://localhost:3000/weather?address=Alameda').then((response) => {
-  response.json().then((data) => {
-    if(data.error){
-      console.log(data.error)
-    } else {
-      console.log(data.location)
-      console.log(data.forecast)
-    }
+// fetch('http://localhost:3000/weather?address=Alameda').then((response) => {
+//   response.json().then((data) => {
+//     if(data.error){
+//       console.log(data.error)
+//     } else {
+//       console.log(data.location)
+//       console.log(data.forecast)
+//     }
+//   })
+// })
+
+const weatherFrom = document.querySelector('form')
+
+const search = document.querySelector('input')
+
+weatherFrom.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const location = search.value
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if(data.error){
+        console.log(data.error)
+      } else {
+        console.log(data.location)
+        console.log(data.forecast)
+      }
+    })
   })
+
+
+
+
+
 })
 
